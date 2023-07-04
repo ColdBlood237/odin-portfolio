3ed4dcfa3dcb753a3fb7072b3a9ef01a2e0d6132
Pixel <petngalan@gmail.com>
2023-07-04 23:47:52 +0200

    Write the style

--- src/App.js
+++ src/App.js
@@ -1,95 +1,119 @@
 import Project from "./Project";
 import { Github, Linkedin, Mail, Phone } from "lucide-react";
+import whiteCat from "./images/white-cat.jpg";
+import blackCat from "./images/black-cat.jpg";
+import projectPic from "./images/coding.jpg";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <img src="/images/white-cat.jpg" alt="" />
-        <h1>Ryan Petnga</h1>
+        <div className="profile-pic-container">
+          <img src={whiteCat} alt="" className="profile-pic" />
+          <h1 className="my-name">Ryan Petnga</h1>
+        </div>
         <div className="presentation">
-          <h2 className="text-3xl font-bold underline">About me</h2>
+          <h2>About me</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
             pretium arcu nec tortor efficitur eleifend. Cras dictum malesuada
             mi, at porta est lobortis non. Mauris sodales bibendum felis, id
             lacinia dolor laoreet ac. Sed sed nibh elit. In sit amet dui in
-            dolor fringilla posuere. Vestibulum ante ipsum primis in faucibus
-            orci luctus et ultrices posuere cubilia Curae; Duis vitae lacus id
-            metus ullamcorper tristique. Nullam vitae purus semper, dignissim
-            est eu, tempor tellus. Morbi malesuada purus at metus bibendum
           </p>
           <div className="presentation-socials">
-            <Github />
-            <Linkedin />
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/ColdBlood237"
+            >
+              <Github />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/ryan-petnga"
+            >
+              <Linkedin />
+            </a>
           </div>
         </div>
       </header>
       <main>
         <h2>My work</h2>
-        <Project
-          name="Project name"
-          description="Short description of the project. Just a couple sentences will do."
-          imgURL="/images/coding.jpg"
-        />
-        <Project
-          name="Project name"
-          description="Short description of the project. Just a couple sentences will do."
-          imgURL="/images/coding.jpg"
-        />
-        <Project
-          name="Project name"
-          description="Short description of the project. Just a couple sentences will do."
-          imgURL="/images/coding.jpg"
-        />
-        <Project
-          name="Project name"
-          description="Short description of the project. Just a couple sentences will do."
-          imgURL="/images/coding.jpg"
-        />
-        <Project
-          name="Project name"
-          description="Short description of the project. Just a couple sentences will do."
-          imgURL="/images/coding.jpg"
-        />
-        <Project
-          name="Project name"
-          description="Short description of the project. Just a couple sentences will do."
-          imgURL="/images/coding.jpg"
-        />
+        <div className="projects-container">
+          <Project
+            name="Project name"
+            description="Short description of the project. Just a couple sentences will do."
+            imgURL={projectPic}
+          />
+          <Project
+            name="Project name"
+            description="Short description of the project. Just a couple sentences will do."
+            imgURL={projectPic}
+          />
+          <Project
+            name="Project name"
+            description="Short description of the project. Just a couple sentences will do."
+            imgURL={projectPic}
+          />
+          <Project
+            name="Project name"
+            description="Short description of the project. Just a couple sentences will do."
+            imgURL={projectPic}
+          />
+          <Project
+            name="Project name"
+            description="Short description of the project. Just a couple sentences will do."
+            imgURL={projectPic}
+          />
+          <Project
+            name="Project name"
+            description="Short description of the project. Just a couple sentences will do."
+            imgURL={projectPic}
+          />
+        </div>
       </main>
       <footer>
-        <h2 className="contact-me">Contact me</h2>
-        <p>
-          Please get in touch if you think my work could be mutually beneficial!
-        </p>
-        <p className="address">Via Anna Frank, 17, 10072 Mappano TO, Italy</p>
-
-        <p className="phone">
-          <Phone /> +39 389 994 8706
-        </p>
-        <p className="mail">
-          <Mail />{" "}
-          <a target="_blank" rel="noreferrer" href="mailto:petngalan@gmail.com">
-            petngalan@gmail.com
-          </a>
-        </p>
-        <div className="socials">
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/ColdBlood237"
-          >
-            <Github />
-          </a>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/ryan-petnga"
-          >
-            <Linkedin />
-          </a>
+        <div className="footer-text">
+          <h2 className="contact-me">Contact me</h2>
+          <p>
+            Please get in touch if you think my work could be mutually
+            beneficial!
+          </p>
+          <p className="address">Via Anna Frank, 17, 10072 Mappano TO, Italy</p>
+          <div className="contacts">
+            <p className="phone">
+              <Phone /> +39 389 994 8706
+            </p>
+            <p className="mail">
+              <Mail />{" "}
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="mailto:petngalan@gmail.com"
+              >
+                petngalan@gmail.com
+              </a>
+            </p>
+          </div>
+          <div className="socials">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/ColdBlood237"
+            >
+              <Github />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/ryan-petnga"
+            >
+              <Linkedin />
+            </a>
+          </div>
         </div>
+        <img src={whiteCat} alt="" className="footer-img" />
       </footer>
     </div>
   );
--- src/Project.js
+++ src/Project.js
@@ -2,20 +2,22 @@ import { ExternalLink, Github } from "lucide-react";
 
 function Project({ name, description, imgURL, liveCodeURL, repositoryURL }) {
   return (
-    <div>
+    <div className="project-card">
       <img src={imgURL} className="project-screenshot" alt="" />
-      <div className="project-header">
-        <h3 className="project-name">{name}</h3>
-        <div className="project-links">
-          <a target="_blank" rel="noreferrer" href={repositoryURL}>
-            <Github />
-          </a>
-          <a target="_blank" rel="noreferrer" href={liveCodeURL}>
-            <ExternalLink />
-          </a>
+      <div className="project-info">
+        <div className="project-header">
+          <h3 className="project-name">{name}</h3>
+          <div className="project-links">
+            <a target="_blank" rel="noreferrer" href={repositoryURL}>
+              <Github />
+            </a>
+            <a target="_blank" rel="noreferrer" href={liveCodeURL}>
+              <ExternalLink />
+            </a>
+          </div>
         </div>
+        <p className="project-description">{description}</p>
       </div>
-      <p className="project-description">{description}</p>
     </div>
   );
 }
